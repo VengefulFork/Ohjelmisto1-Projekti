e4e6cd7bbe020bb81cee6eb32a8c8227225e9029
lauri <lauri.paloheimo@live.com>
2025-03-05 16:39:52 +0200

    Luurangon kehittämistä pidemmälle

--- Funktiot/Pääohjelma.py
+++ Funktiot/Pääohjelma.py
@@ -1,17 +1,18 @@
 from Reitinluoja import reitinluoja
 from Lentäminen import lentäminen
 
+maali = reitinluoja()[1]
+pelaajan_sijainti = reitinluoja()[0]
+ensimmäinen_toisto = True
 
+while maali[0][1] != pelaajan_sijainti[1] :
 
-while maali[0][1] != pelaajan_sijainti :
-        ensimmäinen_toisto = True
         if ensimmäinen_toisto :
             pelaajan_nimi = input("Anna nimesi = ")
-            pelaajan_sijainti = reitinluoja()[0]
-            maali = reitinluoja()[1]
-            print (f"Hei {pelaajan_nimi}")
+            print(f"Hei {pelaajan_nimi}")
+            print(maali)
             ensimmäinen_toisto = False
-
+        pelaajan_sijainti = lentäminen(pelaajan_sijainti)
 
 
 
