a5ea3bbb4804fecb114ad558b7d2eb381efb16e0
lauri <lauri.paloheimo@live.com>
2025-03-07 09:26:10 +0200

    TK yhteyden muokkaus ja funktioon varsinaiset käytettävät parametrit

--- Funktiot/Etäisyydet.py
+++ Funktiot/Etäisyydet.py
@@ -3,30 +3,30 @@ from geopy import distance
 
 
 tk_yhteys = mariadb.connect(
-        host='localhost',
-        port= 3306,
-        database= 'peli_projekti',
-        user= 'root',
-        password= 'K0ir4Juoks1',
-        autocommit=True
+    host='localhost',
+    port=3306,
+    database='peli_projekti',
+    user='player',
+    password='12345',
+    autocommit=True
 )
 
-def koordinaatit (pelaajan_sijainti, icao1):
-    sql = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '{pelaajan_sijainti[1]}'"
+def koordinaatit (pelaajan_vanha_sijainti, pelaajan_sijainti):
+    sql = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '{pelaajan_vanha_sijainti[1]}'"
     curs = tk_yhteys.cursor()
     curs.execute(sql)
     tulos = curs.fetchall()
     #valittu_kenttä = input("\nAnna kentän ICAO-koodi jolle haluat lentää = ").upper()
-    sql2 = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '{icao1}'"
+    sql2 = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '{pelaajan_sijainti[1]}'"
     curs.execute(sql2)
     tulos2 = curs.fetchall()
     k_etäisyys = round(distance.distance(tulos, tulos2 ).km)
 
     return k_etäisyys
 
-pelaajan_sijainti = ["OK", "ESSA"]
-
-
-icao1 = "EFHK"
-
-print(koordinaatit(pelaajan_sijainti, icao1))
+# pelaajan_sijainti = ["OK", "ESSA"]
+#
+#
+# icao1 = "EFHK"
+#
+# print(koordinaatit(pelaajan_sijainti, icao1))
