324481043f292e986393fd1c70a54dcfdab91f80
lauri <lauri.paloheimo@live.com>
2025-03-12 10:45:32 +0200

    hiilidioksidi per km

--- Funktiot/lentokoneet.py
+++ Funktiot/lentokoneet.py
@@ -5,7 +5,8 @@ lentokonetyypit = [
         "matkustajamaara": 189,
         "kantama_km": 6570,
         "max_nopeus_kmh": 876,
-        "poltoaineen_kulutus_kg/km": 3.49
+        "poltoaineen_kulutus_kg/km": 3.49,
+        "hiilidioksidi_per_km": 11
     },
     {
         "malli": "Dash 8 Q400",
@@ -13,7 +14,8 @@ lentokonetyypit = [
         "matkustajamaara": 82,
         "kantama_km": 2040,
         "max_nopeus_kmh": 600,
-        "poltoaineen_kulutus_kg/km": 1.83
+        "poltoaineen_kulutus_kg/km": 1.83,
+        "hiilidioksidi_per_km": 5.78
     }
 ]
 # for kone in lentokonetyypit:
